c8da9820454b7deb68863cea04d3a2f796121bfa
Siham <khaddou.siham@gmail.com>
2023-04-20 17:17:26 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * description : print  exactly with proper grammar, but the outcome is a piece of art
+ * description : print 'with proper grammar, but the outcome is a piece of art'
  * Return: Always 0 (Success)
  */
 int main(void)
