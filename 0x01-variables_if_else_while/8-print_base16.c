5b374a3d4deb7e5c4c1ce91768b4ee5bce8f6cd3
Siham <khaddou.siham@gmail.com>
2023-04-24 11:05:27 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int decim;
+char alphabet;
+
+for (decim = '0'; decim <= '9'; decim++)
+{
+putchar(d);
+}
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+
+return (0);
+}
