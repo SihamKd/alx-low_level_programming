aeec7e6fd7dc5da4533a9aa0e0661d15dcbe40e6
Siham <khaddou.siham@gmail.com>
2023-04-24 10:20:27 -0700

    Alphabets lowerCase and UpperCase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,13 +15,11 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-	}
-		
+	}		
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
-	
-	putchar('\n');
+       	putchar('\n');
 	return (0);
 }
